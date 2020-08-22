fc70435b10924a59f70d9fec9c57ff0deef246e2
snow04 <agabriel.nieve@gmail.com>
2020-08-23 00:25:24 +0800

    sidebar updated

--- application/views/includes/admin/sidebar.php
+++ application/views/includes/admin/sidebar.php
@@ -5,10 +5,7 @@
         MY TEMPLATE 
       </div>
       <div class="list-group list-group-flush">
-        <a href="<?=base_url('patient_profile/')?>" class="list-group-item list-group-item-action bg-light"><i class="fas fa-file"></i> Dashboard</a>
-        <a href="<?=base_url('clinic/')?>" class="list-group-item list-group-item-action bg-light"><i class="fa fa-home"></i> Clinic</a>
-        <a href="<?=base_url('patient_doctor/')?>" class="list-group-item list-group-item-action bg-light"><i class="fa fa-users"></i> Doctors</a>
-        <a href="<?=base_url('appointments/')?>" class="list-group-item list-group-item-action bg-light"><i class="fa fa-pencil"></i> Appointments</a>
+        <a href="<?=base_url('profile/')?>" class="list-group-item list-group-item-action bg-light"><i class="fas fa-file"></i> Dashboard</a>
       </div>
     </div>
     <!-- /#sidebar-wrapper -->

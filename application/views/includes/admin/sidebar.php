c6b0cb7d114728ca85172dbf3521bb5bd9a03ca9
snow04 <agabriel.nieve@gmail.com>
2020-08-22 23:54:38 +0800

    init

---
+++ application/views/includes/admin/sidebar.php
@@ -0,0 +1,14 @@
+<!-- Sidebar -->
+<div class="bg-light border-right" id="sidebar-wrapper">
+      <div class="sidebar-heading">
+        <img src="<?=base_url('support/images/logo.png');?>" style="width:50px"; alt="">
+        MY TEMPLATE 
+      </div>
+      <div class="list-group list-group-flush">
+        <a href="<?=base_url('patient_profile/')?>" class="list-group-item list-group-item-action bg-light"><i class="fas fa-file"></i> Dashboard</a>
+        <a href="<?=base_url('clinic/')?>" class="list-group-item list-group-item-action bg-light"><i class="fa fa-home"></i> Clinic</a>
+        <a href="<?=base_url('patient_doctor/')?>" class="list-group-item list-group-item-action bg-light"><i class="fa fa-users"></i> Doctors</a>
+        <a href="<?=base_url('appointments/')?>" class="list-group-item list-group-item-action bg-light"><i class="fa fa-pencil"></i> Appointments</a>
+      </div>
+    </div>
+    <!-- /#sidebar-wrapper -->
